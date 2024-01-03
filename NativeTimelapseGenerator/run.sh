ac90d80fc1a5f2162bfe11a5f97ac68d3872c10c
Zekiah-A <zekiahamoako@outlook.com>
2024-01-03 00:56:56 +0000

    Fix run shell script

--- NativeTimelapseGenerator/run.sh
+++ NativeTimelapseGenerator/run.sh
@@ -4,4 +4,4 @@ dotnet publish -r linux-x64 -c Release
 cd ../NativeTimelapseGenerator
 cp -r ../NativeTimelapseGenerator.Console/bin/Release/net8.0/linux-x64/publish/* .
 gcc -o NativeTimelapseGenerator -lpng -lcurl main.c image-generator.c image-generator.h canvas-downloader.c canvas-downloader.h console.c console.h
-./generator
+./NativeTimelapseGenerator
